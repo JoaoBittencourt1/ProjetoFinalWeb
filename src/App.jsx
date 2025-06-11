55a99633a404ee9133b3126d926f13871faaef45
joao <jvabgo@gmail.com>
2025-06-11 17:42:40 -0300

    fix: arrumando as rotas do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginContainer from './components/Logincontainer/Logincontainer';
 import RegisterContainer from './components/RegisterContainer/RegisterContainer';
-import HomePrototype from './components/HomePrototype/HomePrototype'; // <- import novo
+import HomePrototype from './components/HomePrototype/HomePrototype';
 
 export default function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
                 <Route path="/login" element={<LoginContainer />} />
                 <Route path="/register" element={<RegisterContainer />} />
-                <Route path="/home-prototype" element={<HomePrototype />} /> {/* <- nova rota */}
-                <Route path="*" element={<LoginContainer />} />
+                <Route path="/home-prototype" element={<HomePrototype />} />
+                <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
         </Router>
     );
